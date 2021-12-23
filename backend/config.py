435b65cdf5f3828148e07a62a25dbd6f2fd0087d
Gilad Spitzer <52394657+giladspitzer@users.noreply.github.com>
2021-12-23 00:06:13 -0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,11 @@
 
 class Config:
-    SECRET_KEY = 'gilad'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI="sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
-    SPOTIFY_API_KEY = '***REMOVED***'
-    SPOTIFY_API_SECRET = '***REMOVED***'
+    SPOTIFY_API_KEY = os.environ['SPOTIFY_API_KEY']
+    SPOTIFY_API_SECRET = os.environ['SPOTIFY_API_SECRET']
     TEMPLATE_FOLDER = '../frontend/dist'
     CORS_ALLOW_HEADERS = '*'
     CORS_ORIGINS = '*'
